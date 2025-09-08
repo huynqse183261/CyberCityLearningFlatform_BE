2607a3414c6a0bb149268f2f05be1ce32e3babc6
huynqse183261 <huynqse183261@fpt.edu.vn>
2025-09-08 08:48:53 +0700

    add docker file

--- CyberCity.Infrastructure/SubtopicRepo.cs
+++ CyberCity.Infrastructure/SubtopicRepo.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CyberCity.Doman.DBcontext;
 using CyberCity.Doman.Models;
 using CyberCity.Infrastructure.Basic;
+using Microsoft.EntityFrameworkCore;
 
 namespace CyberCity.Infrastructure
 {
@@ -15,7 +16,9 @@ namespace CyberCity.Infrastructure
         public SubtopicRepo(CyberCityLearningFlatFormDBContext context) => _context = context;
         public IQueryable<Subtopic> GetAllAsync(bool descending = true)
         {
-            var query = _context.Subtopics.AsQueryable();
+            var query = _context.Subtopics
+                .Include(t => t.TopicU)
+                .AsQueryable();
             return descending
                 ? query.OrderByDescending(c => c.CreatedAt)
                 : query.OrderBy(c => c.CreatedAt);
